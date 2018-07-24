ae3c157cc2978e72106e17100b424b64454f4911
Dexter H <league@WTS14.attlocal.net>
2018-07-23 18:26:00 -0700

    LeagueInvaders(IP)

---
+++ src/LeagueInvaders/Runner.java
@@ -0,0 +1,22 @@
+package LeagueInvaders;
+
+import javax.swing.JFrame;
+
+public class Runner {
+	JFrame frame;
+	final int WIDTH = 500;
+	final int HIEGHT = 800;
+
+	public Runner() {
+		frame = new JFrame();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	void setup() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}

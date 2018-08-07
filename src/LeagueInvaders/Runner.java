8e690865ef3df25093c3887c5fa70d25d9c782cf
kzhuo123 <league@WTS14.attlocal.net>
2018-08-06 18:25:19 -0700

    leagueInvaders(IP)

--- src/LeagueInvaders/Runner.java
+++ src/LeagueInvaders/Runner.java
@@ -1,22 +1,33 @@
 package LeagueInvaders;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Runner {
 	JFrame frame;
 	final int WIDTH = 500;
 	final int HIEGHT = 800;
+	static GamePanel gamepanel;
 
 	public Runner() {
 		frame = new JFrame();
 	}
 
 	public static void main(String[] args) {
-
+		Runner run = new Runner();
+		gamepanel = new GamePanel();
+		run.setup();
 	}
 
 	void setup() {
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(gamepanel);
+		frame.addKeyListener(gamepanel);
+		frame.setSize(WIDTH, WIDTH);
+		frame.getContentPane().setPreferredSize(new Dimension(WIDTH, HIEGHT));
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gamepanel.startGame();
 	}
 }
